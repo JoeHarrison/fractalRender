879265b6381267e395aa414ff00ca84b9cd1aeaf
JoeHarrison <JoeHarrison1994@gmail.com>
2019-04-20 16:50:31 +0200

    Loading image in js

--- script.js
+++ script.js
@@ -3,34 +3,62 @@ const gpu = new GPU({
     canvas: document.getElementById('ctx')
 });
 
-
-const gpuDrawRandom = gpu.createKernel(function(max_iter, colors, k, x_offset, y_offset){
+const renderMandelbrot = gpu.createKernel(function(max_iter, colors, zoom, x_offset, y_offset, x_pan, y_pan, image){
     var x = 0;
     var y = 0;
     var xx = 0;
     var yy = 0;
     var xy = 0;
 
-    var c1 = -1 + (2*k)/512.0*(float(this.thread.x) - x_offset);
-    var c2 = -1 + (2*k)/512.0*(float(this.thread.y) + y_offset);
+    var c1 = (1/zoom)*(float(this.thread.x) + x_offset + x_pan);
+    var c2 = (1/zoom)*(float(this.thread.y) + y_offset - y_pan);
 
     var i = 0;
+    this.color(0, 0, 0);
     while(i<max_iter){
         xy = x*y;
         xx = x*x;
         yy = y*y;
-        x = xx - yy + c1;
-        y = 2*xy + c2;
         if((xx + yy) >= 4.0){
+            this.color(colors[i][0], colors[i][1], colors[i][2]);
             break;
         }
+        x = xx - yy + c1;
+        y = 2*xy + c2;
         i++;
     }
 
-    this.color(colors[i][0], colors[i][1], colors[i][2]);
+
+
 }).setGraphical(true).setOutput([512, 512]);
 
-let max_iter = 32;
+const renderPointOrbitTrap = gpu.createKernel(function(max_iter, colors, zoom, x_offset, y_offset, x_pan, y_pan, image){
+    var x = 0;
+    var y = 0;
+    var xx = 0;
+    var yy = 0;
+    var xy = 0;
+
+    var c1 = (1/zoom)*(float(this.thread.x) + x_offset + x_pan);
+    var c2 = (1/zoom)*(float(this.thread.y) + y_offset - y_pan);
+
+    var i = 0;
+    this.color(0, 0, 0);
+    while(i<max_iter){
+        xy = x*y;
+        xx = x*x;
+        yy = y*y;
+        if((xx + yy) >= 4.0){
+            this.color(colors[i][0], colors[i][1], colors[i][2]);
+            break;
+        }
+        x = xx - yy + c1;
+        y = 2*xy + c2;
+        i++;
+    }
+}).setGraphical(true).setOutput([512, 512]);
+
+let max_iter = 250;
 
 var colors = [];
 
@@ -40,27 +68,73 @@ for(var i = 0; i<max_iter; i++){
     colors.push([color.r/255.0, color.g/255.0, color.b/255.0]);
 }
 
+var canvas = document.getElementById("ctx");
+var ctx = canvas.getContext("2d");
 
-let zoom = d3.zoom()
-.on('zoom', zoomed)
+canvas.addEventListener("mousedown", onMouseDown);
 
-previous_k = 1;
+// Width and height of the image
+var imagew = 512.0;
+var imageh = 512.0;
 
-function zoomed(){
-    console.log(d3.event.transform.k, previous_k)
-    if((d3.event.transform.k - previous_k)**2 > 0){
-        gpuDrawRandom(max_iter, colors, d3.event.transform.k, d3.event.transform.x, d3.event.transform.y);
-    }
-    else{
-        gpuDrawRandom(max_iter, colors, d3.event.transform.k, d3.event.transform.x, d3.event.transform.y);
+// Pan and zoom parameters
+var offsetx = -imagew/2;
+var offsety = -imageh/2;
+var panx = -100;
+var pany = 0;
+var zoom = 150;
+
+function zoomFractal(x, y, factor, zoomin) {
+    if (zoomin) {
+        // Zoom in
+        zoom *= factor;
+        panx = factor * (x + offsetx + panx);
+        pany = factor * (y + offsety + pany);
+    } else {
+        // Zoom out
+        zoom /= factor;
+        panx = (x + offsetx + panx) / factor;
+        pany = (y + offsety + pany) / factor;
     }
 }
+
+function onMouseDown(e) {
+    if(e.button==0){
+        var pos = getMousePos(canvas, e);
+
+        // Zoom out with Control
+        var zoomin = true;
+        if (e.ctrlKey) {
+            zoomin = false;
+        }
+
+        // Pan with Shift
+        var zoomfactor = 2;
+        if (e.shiftKey) {
+            zoomfactor = 1;
+        }
+
+        // Zoom the fractal at the mouse position
+        zoomFractal(pos.x, pos.y, zoomfactor, zoomin);
+
+        // Generate a new image
+        requestAnimationFrame(draw);
+    }
+}
+
+function getMousePos(canvas, e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width),
+        y: Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height)
+    };
+}
+
+image = [[[0,0,0],[1,1,1],[0,0,0]],[[1,1,1],[1,1,1],[1,1,1]],[[0,1,0],[0,1,0],[0,1,0]]];
 
 function draw(){
-    gpuDrawRandom(max_iter, colors, 1, 0, 0);
-    // requestAnimationFrame(draw);
+    //renderPointOrbitTrap(max_iter, colors, zoom, offsetx, offsety, panx, pany, image);
+    renderMandelbrot(max_iter, colors, zoom, offsetx, offsety, panx, pany, image);
 }
-
-d3.select("#ctx").call(zoom)
 
 requestAnimationFrame(draw);
